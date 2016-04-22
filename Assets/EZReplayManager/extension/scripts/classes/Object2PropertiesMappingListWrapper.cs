2061516004b620fb8faf597ec2a7290eec333103
Ethan <yishionwang314@163.com>
2016-04-22 17:38:40 +0800

    修改 Object2PropertiesMappingListWrapper

--- Assets/EZReplayManager/extension/scripts/classes/Object2PropertiesMappingListWrapper.cs
+++ Assets/EZReplayManager/extension/scripts/classes/Object2PropertiesMappingListWrapper.cs
@@ -3,38 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System;
+using ProtoBuf;
 
-[Serializable()]
-public class Object2PropertiesMappingListWrapper : ISerializable {
-	
-	public List<Object2PropertiesMapping> object2PropertiesMappings = new List<Object2PropertiesMapping>();
+//[Serializable()]
+[ProtoContract]
+public class Object2PropertiesMappingListWrapper// : ISerializable {
+{
+	[ProtoMember (0)]
+	public List<Object2PropertiesMapping> object2PropertiesMappings = new List<Object2PropertiesMapping> ();
+	[ProtoMember (1)]
 	public string EZR_VERSION = EZReplayManager.EZR_VERSION;
+	[ProtoMember (2)]
 	public float recordingInterval;
 
 	//serialization constructor
-	protected Object2PropertiesMappingListWrapper(SerializationInfo info,StreamingContext context) {
-	    object2PropertiesMappings = (List<Object2PropertiesMapping>)info.GetValue("object2PropertiesMappings",typeof(List<Object2PropertiesMapping>));
-		EZR_VERSION = info.GetString("EZR_VERSION");
-		recordingInterval = (float)info.GetValue("recordingInterval",typeof(float));
-	}
-	
-	public Object2PropertiesMappingListWrapper(List<Object2PropertiesMapping> mappings) {
+	//	protected Object2PropertiesMappingListWrapper (SerializationInfo info, StreamingContext context)
+	//	{
+	//		object2PropertiesMappings = (List<Object2PropertiesMapping>)info.GetValue ("object2PropertiesMappings", typeof(List<Object2PropertiesMapping>));
+	//		EZR_VERSION = info.GetString ("EZR_VERSION");
+	//		recordingInterval = (float)info.GetValue ("recordingInterval", typeof(float));
+	//	}
+
+	public Object2PropertiesMappingListWrapper (List<Object2PropertiesMapping> mappings)
+	{
 		object2PropertiesMappings = mappings;
 	}
-	
-	public Object2PropertiesMappingListWrapper() {
+
+	public Object2PropertiesMappingListWrapper ()
+	{
 
 	}
-	
-	public void addMapping(Object2PropertiesMapping mapping) {
-		object2PropertiesMappings.Add(mapping);
-	}
-	
-	public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+
+	public void addMapping (Object2PropertiesMapping mapping)
 	{
-		info.AddValue("object2PropertiesMappings", this.object2PropertiesMappings);
-		info.AddValue("EZR_VERSION", EZR_VERSION);
-		info.AddValue("recordingInterval", this.recordingInterval);
-		//base.GetObjectData(info, context);
-	}	
+		object2PropertiesMappings.Add (mapping);
+	}
+
+	//	public void GetObjectData (SerializationInfo info, StreamingContext ctxt)
+	//	{
+	//		info.AddValue ("object2PropertiesMappings", this.object2PropertiesMappings);
+	//		info.AddValue ("EZR_VERSION", EZR_VERSION);
+	//		info.AddValue ("recordingInterval", this.recordingInterval);
+	//		//base.GetObjectData(info, context);
+	//	}
 }
